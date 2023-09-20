97511a731c0b608350c2c61241d15724876642a0
= <=>
2023-09-19 23:57:10 -0300

    icons rodapé

--- js/script.js
+++ js/script.js
@@ -69,3 +69,7 @@ function showPassword () {
 }
 
 // promoções
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
