9d09a60a62b24090d3acc3b74336c0ae11933ed7
= <=>
2023-09-20 23:54:48 -0300

    feito pagina cadastro e login

--- js/script.js
+++ js/script.js
@@ -73,3 +73,5 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+
